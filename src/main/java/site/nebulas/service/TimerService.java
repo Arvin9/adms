c075533b74becc6d9a73034db00ef649cb3d82cd
nebula <594113869@qq.com>
2016-08-25 11:10:19 +0800

    定时器

---
+++ src/main/java/site/nebulas/service/TimerService.java
@@ -0,0 +1,12 @@
+package site.nebulas.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service 
+public class TimerService {
+	 @Scheduled(cron="0/5 * *  * * ? ")   //每5秒执行一次  
+     public void myTest(){  
+           System.out.println("进入测试");  
+     }  
+}
